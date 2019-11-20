506bdcca8dc2bd8092ec86bb761d0309e8859261
venkatkumar18 <51899382+venkatkumar18@users.noreply.github.com>
2019-11-20 11:46:10 +0530

    can you help me

---
+++ helpNeeded.py
@@ -0,0 +1,11 @@
+// Hello sir , I'm a second year college student , i have been broken,unclear about my future i started worrying daily 
+// can you tell me what are things i need to learn , to get a good job in 2022..
+// i'm intrested in python, so i will do more work related to that on additiono could you please tell me what are the things i need to learn
+
+if(telusko.help(me)):
+    thanks()
+    i_will_win()
+    success()
+else:
+    i_will_work()
+    success()
